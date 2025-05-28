0b54bd01a6ee7126da1fcb16b9fd6e161dbf5ed6
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2025-05-28 18:41:06 -0300

    refactor: Move 'RemoveFeedLoader' to Feed API folder

---
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Rhuan Carvalho on 28/05/25.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    private let client: HTTPClient
+    private let url: URL
+    
+    public init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
