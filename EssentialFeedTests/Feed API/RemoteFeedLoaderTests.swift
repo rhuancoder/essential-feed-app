f4cc412a57a77b8d7e5bb2044d78bb919cbf00bd
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2025-10-01 19:50:16 -0300

    test: add memory leak tracking

--- EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -124,7 +124,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         return .failure(error)
     }
     
-    private func trackForMemoryLeaks(_ instance: AnyObject,  file: StaticString = #file, line: UInt = #line) {
+    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
         addTeardownBlock { [weak instance] in
             XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
         }
