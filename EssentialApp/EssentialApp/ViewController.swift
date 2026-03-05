03de0fc4dfeb433d5a765330bdd3e3ff9bf38cd1
Rhuan Carvalho <rhuancoder@gmail.com>
2026-03-04 21:15:52 -0300

    feat: Add empty EssentialApp project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Rhuan Carvalho on 04/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
