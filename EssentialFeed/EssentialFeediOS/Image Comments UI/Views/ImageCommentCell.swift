78ec341fdbab1921da699bc20c6082dbb5f238f3
Rhuan Carvalho <rhuancoder@gmail.com>
2026-03-31 21:32:36 -0300

    feat: Implement Image Comments UI

---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeed
+//
+//  Created by Rhuan Carvalho on 31/03/26.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var messageLabel: UILabel!
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+}
